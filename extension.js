65ec9bac9370497b7a7d309bdefe96b19647f5b9
Jared Corker <jared.corker@gmail.com>
2023-07-26 17:22:02 +1000

    v1 Code

--- extension.js
+++ extension.js
@@ -1,33 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	let disposable = vscode.commands.registerCommand('spectre.spectre', function () {
+		const currentFile = vscode.window.activeTextEditor.document.fileName
+		if (!currentFile) return;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "spectre" is now active!');
+		if (currentFile.includes("spec")) {
+			const codeFile = currentFile.replace("spec.", "").replace("__tests__/", "")
+			vscode.window.showTextDocument(vscode.Uri.file(codeFile), { preview: false })
+		} else {
+			const match = currentFile.match(/\/([a-zA-Z]*).(ts|tsx)$/)
+			if (!match) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('spectre.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+			const specFileName = `__tests__/${match[1]}.spec.${match[2]}`
+			const specFilePath = currentFile.replace(match[0], specFileName)
+			vscode.window.showTextDocument(vscode.Uri.file(specFilePath), { preview: false }).then(() => {}, async () => {
+				const answer = await vscode.window.showErrorMessage(`Could not find ${specFileName}`, "Create", "Cancel")	
+				if (answer === "Create") {
+					const wsEdit = new vscode.WorkspaceEdit()
+					wsEdit.createFile(vscode.Uri.file(specFilePath), { ignoreIfExists: true })
+					vscode.workspace.applyEdit(wsEdit).then(() => {
+						vscode.window.showTextDocument(vscode.Uri.file(specFilePath), { preview: false })
+					})
+				}
+			})
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Spectre!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
